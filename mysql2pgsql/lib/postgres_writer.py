9ecfec069f3a6ea2e4be72d70e00a8e52c826ac0
kworr <c.kworr@gmail.com>
2013-11-17 12:09:43 -0800

    probably another case of incorrect entity quoting - they should be quoted with double quotes

--- mysql2pgsql/lib/postgres_writer.py
+++ mysql2pgsql/lib/postgres_writer.py
@@ -126,7 +126,7 @@ class PostgresWriter(object):
         default, column_type = get_type(column)
 
         if column.get('auto_increment', None):
-            return '%s DEFAULT nextval(\'%s_%s_seq\'::regclass) NOT NULL' % (
+            return '%s DEFAULT nextval("%s_%s_seq"::regclass) NOT NULL' % (
                    column_type, column['table_name'], column['name'])
                     
         return '%s%s%s' % (column_type, (default if not default == None else ''), null)
