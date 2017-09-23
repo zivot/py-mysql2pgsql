6d8e232dc76084b69ab4dd76149a2b7c81c0e2b9
Volodymyr Kostyrko <c.kworr@gmail.com>
2017-09-23 21:45:10 +0300

    Don't execute empty statements.

--- mysql2pgsql/lib/postgres_db_writer.py
+++ mysql2pgsql/lib/postgres_db_writer.py
@@ -102,6 +102,8 @@ class PostgresDbWriter(PostgresWriter):
             return cur.fetchone() if one else cur
 
     def execute(self, sql, args=(), many=False):
+        if len(sql) == 0:
+            return
         with closing(self.conn.cursor()) as cur:
             if many:
                 cur.executemany(sql, args)
